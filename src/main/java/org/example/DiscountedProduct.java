5a251ac1a06ef3c3bf371881e1201215adc6c4c2
hector918 <hector_zhong@outlook.com>
2025-04-07 15:34:02 -0400

    finished the test

--- src/main/java/org/example/DiscountedProduct.java
+++ src/main/java/org/example/DiscountedProduct.java
@@ -1,5 +1,13 @@
 package org.example;
 
 public class DiscountedProduct extends Product {
+    public double discountPercentage;
+    public DiscountedProduct(String name, double price, String description, double discountPercentage){
+        super(name, price, description);
+        this.discountPercentage = discountPercentage;
+    }
 
+    public double calculateDiscountedPrice(){
+        return this.getPrice() * (1 - discountPercentage/100);
+    }
 }
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,5 +1,30 @@
 package org.example;
 
 public class Product {
+    private String name;
+    private double price;
+    private String desciption;
 
+
+    public Product(String name, double price, String desciption){
+        this.name = name;
+        this.price = price;
+        this.desciption = desciption;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public String getDescription(){
+        return this.desciption;
+    }
+
+    public void displayProductInfo(){
+
+    }
 }
